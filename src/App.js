8cd70c01b6bc77dcf7079841dfd4ca9ee59eb852
John Zhou <chowjohn10@gmail.com>
2023-10-27 13:57:24 -0400

    Created divs to organize gridmaker and added useState function in addRow
    Changes made:
    
    - Added import statement for useState
    - Added numRows variable and setNumRows for row number access
    - Created 2 divs: grid-tools and grid-row
    - Implemented row incrementation on button click, but not complete yet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import { useState } from "react";
+
 export default function Gridmaker() {
+  const [numRows, setNumRows] = useState(0);
+
   function addRow() {
-    return;
+    setNumRows(numRows + 1);
+    console.log(`numRows incremented to ${numRows}`);
   }
 
-  return <AddRowButton onAddRowButtonClick={()=> addRow() } />
+  return (
+    <>
+      <div className="grid-tools">
+        <AddRowButton onAddRowButtonClick={() => addRow()} />
+      </div>
+
+      <div className="grid-row"></div>
+    </>
+  );
 }
 
 function AddRowButton({ onAddRowButtonClick }) {
-  return <button className="AddRowButton" onClick={onAddRowButtonClick}>Add Row</button>;
+  return (
+    <button className="add-row-button" onClick={onAddRowButtonClick}>
+      Add Row
+    </button>
+  );
 }
